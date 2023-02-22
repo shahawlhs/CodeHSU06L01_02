fad6b6e26efd2d1fe32d236065975e8acfdebde2
Brandon Smith <smithb@wlwv.k12.or.us>
2023-02-22 10:35:39 -0800

    intitial commit

---
+++ src/ArrayFun.java
@@ -0,0 +1,37 @@
+public class ArrayFun {
+
+    public static void main(String[] args) {
+        //declare an empty double array of size 6
+
+        //declare an array of 10 ints
+
+        //how are these elements organized?
+
+        //getting a value
+
+
+        //replacing a value
+
+
+        //getting the length of an array
+
+
+        ///////////////traversing arrays/////////////////
+
+        //printing all the values in an array
+
+        //while loop
+
+
+
+
+        //for loop
+
+
+
+
+        //printing
+
+
+    }
+}
